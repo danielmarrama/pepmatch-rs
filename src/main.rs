1f6b98870a96a1a068e387f9800fe48a1fe2126a
Daniel Marrama <dmarrama@lji.org>
2023-03-24 00:06:43 -0700

    create kmers table and writer kmers and indices

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use rusqlite;
 
-// split the peptide in k-mers with a window size of 1
-fn split_sequence(seq: &str, k: usize) -> Vec<String> {
+// split the peptide into k-mers with a window size of 1 and store also the index of that k-mer
+fn split_sequence(seq: &str, k: usize) -> Vec<(String, usize)> {
     let mut kmers = Vec::new();
     let mut i = 0;
     while i < seq.len() - k + 1 {
-        kmers.push(seq[i..i + k].to_string());
+        kmers.push((seq[i..i + k].to_string(), i));
         i += 1;
     }
     kmers
@@ -17,9 +17,35 @@ fn connect() -> rusqlite::Connection {
     conn
 }
 
-fn main() {
-    let peptide = "YLLDLHSYL";
-    let kmers = split_sequence(peptide, 3);
-    println!("{:?}", kmers);
-    connect();
+// create a kmers --> index table in the db
+fn create_table(conn: &rusqlite::Connection) {
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS kmers (
+                  kmer            TEXT NOT NULL,
+                  idx             INTEGER NOT NULL
+                  )",
+        rusqlite::params![],
+    )
+    .unwrap();
 }
+
+// insert kmers into the table
+fn insert_kmers(conn: &rusqlite::Connection, kmers: Vec<(String, usize)>) {
+    let mut stmt = conn
+        .prepare("INSERT INTO kmers (kmer, idx) VALUES (?1, ?2)")
+        .unwrap();
+    for kmer in kmers {
+        stmt.execute(rusqlite::params![kmer.0, kmer.1])
+            .unwrap();
+    }
+}
+
+fn main() {
+    let protein = "MLPGLALLLLAAWTARALEVPTDGNAGLLAEPQIAMFCGRLNMHMNVQNGKWDSDPSGTK";
+    let kmers = split_sequence(protein, 3);
+    println!("{:?}", kmers);
+    let conn = connect();
+    create_table(&conn);
+    insert_kmers(&conn, kmers);
+}
+
