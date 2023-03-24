a15b922c5120e484013810f301b7123f16aa6365
Daniel Marrama <danielmarrama@gmail.com>
2023-03-23 22:50:08 -0700

    sqlite connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rusqlite;
+
 // split the peptide in k-mers with a window size of 1
 fn split_sequence(seq: &str, k: usize) -> Vec<String> {
     let mut kmers = Vec::new();
@@ -9,8 +11,15 @@ fn split_sequence(seq: &str, k: usize) -> Vec<String> {
     kmers
 }
 
+// connect to sqlite db, call it proteome.db
+fn connect() -> rusqlite::Connection {
+    let conn = rusqlite::Connection::open("proteome.db").unwrap();
+    conn
+}
+
 fn main() {
     let peptide = "YLLDLHSYL";
     let kmers = split_sequence(peptide, 3);
-    println!("{:?}", kmers);    
+    println!("{:?}", kmers);
+    connect();
 }
